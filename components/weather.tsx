22f87155d8368e73950b38da0c04250ef1d4180b
Owen McComas <owen@mccomas.me>
2023-06-26 14:33:41 -0700

    Weather saves to local storage now

--- components/weather.tsx
+++ components/weather.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
@@ -42,9 +42,20 @@ const WeatherWidget: React.FC = () => {
   const [currentPeriod, setCurrentPeriod] = useState<WeatherPeriod | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [city, setCity] = useState<string>("");
-  const [state, setState] = useState<string>("");
-  const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
+  const [city, setCity] = useState<string>(() => localStorage.getItem("city") || "");
+  const [state, setState] = useState<string>(() => localStorage.getItem("state") || "");
+  const [formSubmitted, setFormSubmitted] = useState<boolean>(() => {
+    const submitted = localStorage.getItem("formSubmitted");
+    return submitted === "true";
+  });
+
+  useEffect(() => {
+    // Save the city and state to local storage whenever they change
+    localStorage.setItem("city", city);
+    localStorage.setItem("state", state);
+    // Save the formSubmitted flag to local storage
+    localStorage.setItem("formSubmitted", String(formSubmitted));
+  }, [city, state, formSubmitted]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -80,50 +91,56 @@ const WeatherWidget: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    if (formSubmitted) {
+      const submitEvent = new Event("submit");
+      handleSubmit(submitEvent as unknown as React.FormEvent);
+    }
+  }, []);
+
   return (
-<div className="flex justify-end">
-  {!formSubmitted ? (
-    <form onSubmit={handleSubmit} className="p-1">
-      <Input
-        type="text"
-        placeholder="Enter city"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        className="p-2 border border-gray-300 rounded"
-      />
-      <Input
-        type="text"
-        placeholder="Enter state"
-        value={state}
-        onChange={(e) => setState(e.target.value)}
-        className="p-2 border border-gray-300 rounded"
-      />
-      <Button
-        type="submit"
-        className="px-4 py-2 text-white rounded"
-        variant={"outline"}
-      >
-        Get Weather
-      </Button>
-    </form>
-  ) : null}
-
-  {loading ? (
-    <div className="text-gray-500">Loading weather data...</div>
-  ) : error ? (
-    <div className="text-red-500">{error}</div>
-  ) : currentPeriod ? (
-    <div className="space-y-2">
-      <div className="text-lg font-semibold">
-        {currentPeriod.temperature}&deg;F
-      </div>
-      <div>{currentPeriod.shortForecast}</div>
+    <div className="flex justify-end">
+      {formSubmitted ? (
+        loading ? (
+          <div className="text-gray-500">Loading weather data...</div>
+        ) : error ? (
+          <div className="text-red-500">{error}</div>
+        ) : currentPeriod ? (
+          <div className="space-y-2">
+            <div className="text-lg font-semibold">
+              {currentPeriod.temperature}&deg;F
+            </div>
+            <div>{currentPeriod.shortForecast}</div>
+          </div>
+        ) : (
+          <div className="text-gray-500">No weather data available</div>
+        )
+      ) : (
+        <form onSubmit={handleSubmit} className="p-1">
+          <Input
+            type="text"
+            placeholder="Enter city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            className="p-2 border border-gray-300 rounded"
+          />
+          <Input
+            type="text"
+            placeholder="Enter state"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+            className="p-2 border border-gray-300 rounded"
+          />
+          <Button
+            type="submit"
+            className="px-4 py-2 text-white rounded"
+            variant={"outline"}
+          >
+            Get Weather
+          </Button>
+        </form>
+      )}
     </div>
-  ) : (
-    <div className="text-gray-500">No weather data available</div>
-  )}
-</div>
-
   );
 };
 
